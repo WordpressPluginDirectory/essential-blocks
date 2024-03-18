6b559aea75982b0bc601dc766c595acdc1c1d3e3
Justin Gardner <justin@rhynorater.com>
2024-03-18 06:03:04 -0500

    4.5.4

--- essential-blocks/admin/index.asset.php
+++ essential-blocks/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '50eb19aa679ce5ac1626');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '58a116fdeeb091535a22');
